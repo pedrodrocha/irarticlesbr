4bec97620f53e801a1c46e2f6bbef9eb3cdfebb4
Pedro Diniz Rocha <pedro.diniz.rocha@gmail.com>
2022-01-28 16:38:17 -0300

    atualizando função de coleta da monções

--- R/download_moncoes.R
+++ R/download_moncoes.R
@@ -12,26 +12,24 @@
 #' @examples
 #' download_moncoes(year = 2020, volume = 9, number = 17, dir = "/home/std_pedrorocha/Documentos/irarticlesbr/teste")
 download_moncoes <- function(year, volume, number, dir, info_data = FALSE) {
-  url_archive <- "http://periodicos.pucminas.br/index.php/estudosinternacionais/issue/archive"
 
   #/ Part I: Retrieve Issues From Archive and Filter from User Input
   usethis::ui_todo('Retrieving issues from archive and filtering based on your input')
 
-  url_archive <- "http://ojs.ufgd.edu.br/index.php/moncoes/issue/archive"
+  url_archive <- "https://ojs.ufgd.edu.br/index.php/moncoes/issue/archive"
 
   url_archive_lido <- xml2::read_html(url_archive)
 
-  url_archive_lido %>%
-    rvest::html_nodes("h4") %>%
-    rvest::html_nodes("a") %>%
-    rvest::html_attr("href") -> primary_url
-
-
-  url_archive_lido %>%
-    rvest::html_nodes("h4") %>%
-    rvest::html_nodes("a") %>%
+  xml2::read_html(url_archive) %>%
+    rvest::html_nodes('.series') %>%
     rvest::html_text() %>%
-    stringr::str_remove(":(.*)") -> eds
+    stringr::str_remove_all("\\n|\\t") -> eds
+
+  xml2::read_html(url_archive) %>%
+    rvest::html_nodes('.obj_issue_summary .title') %>%
+    rvest::html_attr('href') -> primary_url
+
+
 
 
   tibble::tibble(url = primary_url,
@@ -46,7 +44,6 @@ download_moncoes <- function(year, volume, number, dir, info_data = FALSE) {
       ano = stringr::str_extract(editions,"[0-9]{4}") %>%
         as.double(.),
       ano = dplyr::case_when(
-
         vol == 4 ~ 2015,
         vol == 3 ~ 2014,
         vol == 2 ~ 2013,
@@ -58,7 +55,7 @@ download_moncoes <- function(year, volume, number, dir, info_data = FALSE) {
     ) %>%
     dplyr::filter(ano %in% year &
                     n %in% number &
-                    vol %in% volume)-> eds_url
+                    vol %in% volume) -> eds_url
 
 
   usethis::ui_done('Retrieving issues from archive and filtering based on your input')
@@ -70,16 +67,16 @@ download_moncoes <- function(year, volume, number, dir, info_data = FALSE) {
     url_lido <- xml2::read_html(x)
 
     url_lido %>%
-      rvest::html_nodes('.file') %>%
+      rvest::html_nodes('.pdf') %>%
       rvest::html_attr('href') %>%
-      stringr::str_replace(.,"view","download") %>%
-      stringr::str_replace(.,'downloadIssue','download')-> href
-
+      stringr::str_replace(.,"view","download") -> href
     url_lido %>%
-      rvest::html_nodes('h2') %>%
+      rvest::html_nodes('h1') %>%
       rvest::html_text() -> ed
 
+
     pdf_url <- tibble::tibble(url = href, ed = ed)
+
 
     return(pdf_url)
 
