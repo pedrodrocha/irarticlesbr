da399d27c62a22cf0ce629bd52e6ee180831c5ed
Pedro D. Rocha <pedro.diniz.rocha@gmail.com>
2022-03-25 17:36:03 -0300

    Atualizando funço da Austral para OKS 3

--- R/download_austral.R
+++ R/download_austral.R
@@ -12,32 +12,33 @@
 #' @examples
 download_austral <- function(year, volume, number, dir,  info_data = FALSE) {
 
-  url_archive <- "https://seer.ufrgs.br/austral/issue/archive"
 
   #/ Part I: Retrieve Issues From Archive and Filter from User Input
   usethis::ui_todo('Retrieving issues from archive and filtering based on your input')
 
 
+  url_archive <- "https://seer.ufrgs.br/index.php/austral/issue/archive"
   url_archive_lido <- xml2::read_html(url_archive)
 
+
   url_archive_lido %>%
-    rvest::html_nodes("h4 a") %>%
+    rvest::html_nodes(".obj_issue_summary .title") %>%
     rvest::html_attr("href") -> primary_url
 
 
   url_archive_lido %>%
-    rvest::html_nodes("h4 a") %>%
+    rvest::html_nodes(".obj_issue_summary .title") %>%
     rvest::html_text() %>%
-    stringr::str_remove(":(.*)") -> eds
+    stringr::str_trim() -> eds
 
   tibble::tibble(url = primary_url,
                  editions = eds) %>%
     dplyr::mutate(
-      vol = stringr::str_extract(editions, "(Vol [0-9]{2})|(Vol [0-9]{1})") %>%
-        stringr::str_replace_all(.,'Vol ','') %>%
+      vol = stringr::str_extract(editions, "(Vol. [0-9]{2})|(Vol. [0-9]{1})") %>%
+        stringr::str_replace_all(.,'Vol. ','') %>%
         as.integer(.),
-      n = stringr::str_extract(editions,'(No [0-9]{2})|(No [0-9]{1})') %>%
-        stringr::str_replace_all(.,'No ','') %>%
+      n = stringr::str_extract(editions,'(No. [0-9]{2})|(No. [0-9]{1})') %>%
+        stringr::str_replace_all(.,'No. ','') %>%
         as.integer(.),
       ano = stringr::str_extract(editions,"[0-9]{4}") %>%
         as.double(.),
@@ -45,7 +46,8 @@ download_austral <- function(year, volume, number, dir,  info_data = FALSE) {
     ) %>%
     dplyr::filter(ano %in% year &
                     n %in% number &
-                    vol %in% volume) -> eds_url
+                    vol %in% volume)  -> eds_url
+
 
 
 
@@ -54,35 +56,34 @@ download_austral <- function(year, volume, number, dir,  info_data = FALSE) {
   #/ Part II: Retrieve Pdf links
   usethis::ui_todo('Crawling pdfs for download')
   pdfs <- purrr::map_dfr(eds_url$url, function(x) {
+
     url_lido <- xml2::read_html(x)
 
     url_lido %>%
-      rvest::html_nodes('.tocGalleys a') %>%
+      rvest::html_nodes('.pdf') %>%
       rvest::html_attr('href') %>%
-      stringr::str_replace(.,"view","download") %>%
-      stringr::str_replace(.,'downloadIssue','download')-> href
+      stringr::str_replace(.,"view","download") -> href
 
     url_lido %>%
-      rvest::html_nodes('#breadcrumb .current') %>%
-      rvest::html_text() -> ed
+      rvest::html_nodes('h1') %>%
+      rvest::html_text() %>%
+      stringr::str_trim() -> ed
 
     pdf_url <- tibble::tibble(url = href, ed = ed)
 
-
-    return(pdf_url)
+    pdf_url
 
   }) %>%
     dplyr::mutate(
-      vol = stringr::str_extract(ed, "(Vol [0-9]{2})|(Vol [0-9]{1})") %>%
-        stringr::str_replace_all(.,'Vol ','') %>%
+      vol = stringr::str_extract(ed, "(Vol. [0-9]{2})|(Vol. [0-9]{1})") %>%
+        stringr::str_replace_all(.,'Vol. ','') %>%
         as.integer(.),
-      n = stringr::str_extract(ed,'(No [0-9]{2})|(No [0-9]{1})') %>%
-        stringr::str_replace_all(.,'No ','') %>%
+      n = stringr::str_extract(ed,'(No. [0-9]{2})|(No. [0-9]{1})') %>%
+        stringr::str_replace_all(.,'No. ','') %>%
         as.integer(.),
       ano = stringr::str_extract(ed,"[0-9]{4}") %>%
         as.double(.)
     )
-
 
 
 
